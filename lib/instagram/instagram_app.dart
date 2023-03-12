876799ef1886673dd808634dbad79b6be6738527
Yean Seyha <yeanseyha@Yeans-MacBook-Pro.local>
2023-03-12 13:08:50 +0700

    set up module structure

---
+++ lib/instagram/instagram_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/instagram/homepage.dart';
+
+class InstagramApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
