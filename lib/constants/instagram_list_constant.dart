e8b165a39b5beff2d25874502831ff933702a59d
Yean Seyha <yeanseyha@Yeans-MacBook-Pro.local>
2023-03-19 20:48:40 +0700

    Fixed Overflow widget

---
+++ lib/constants/instagram_list_constant.dart
@@ -0,0 +1,16 @@
+import '../models/post_model.dart';
+
+List<PostModel> postList = [
+  PostModel(
+      image:
+          "https://t3.ftcdn.net/jpg/01/66/39/54/360_F_166395402_UcbaS5Z5Tj1rEbMvzhHR1U7DpCgCWd3r.jpg",
+      name: "Ben Dara"),
+  PostModel(
+      image:
+          "https://t3.ftcdn.net/jpg/01/66/39/54/360_F_166395402_UcbaS5Z5Tj1rEbMvzhHR1U7DpCgCWd3r.jpg",
+      name: "James Dara"),
+  PostModel(
+      image:
+          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/charles-leclerc-ferrari-sf-23-during-the-bahrain-grand-prix-news-photo-1678996600.jpg?crop=1.00xw:0.845xh;0,0.155xh&resize=640:*",
+      name: "F1 Ferrai"),
+];
--- lib/instagram/homepage.dart
+++ lib/instagram/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:instagram_clone/constants/instagram_list_constant.dart';
+import 'package:instagram_clone/models/post_model.dart';
 // import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatelessWidget {
@@ -13,8 +15,23 @@ class HomePage extends StatelessWidget {
       // body: SingleChildScrollView(
       //   child: _buildStoryView(),
       // ),
-      body: _buildColumn(),
-      bottomNavigationBar: Text("Customn navigation here!!"),
+      body: SafeArea(child: _buildColumn()),
+      bottomNavigationBar: _buildBottom,
+    );
+  }
+
+  Widget get _buildBottom {
+    return SafeArea(
+      child: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.home)),
+            IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.person)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.more_horiz)),
+          ],
+        ),
+      ),
     );
   }
 
@@ -45,107 +62,73 @@ class HomePage extends StatelessWidget {
   Widget _buildColumn() {
     return Container(
       child: Column(
-        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[_buildStoryView(), _singleInstagramPost()],
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[_buildStoryView(), _buildListViewInstagram()]),
+    );
+  }
+
+  Widget _buildListViewInstagram() {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        height: 600,
+        child: ListView.builder(
+          shrinkWrap: true,
+          itemCount: postList.length,
+          itemBuilder: (context, index) {
+            return _buildSinglePost(postList[index]);
+          },
+        ),
       ),
     );
   }
 
-  Widget _singleInstagramPost() {
+  Widget _buildSinglePost(
+    PostModel item,
+  ) {
     return Container(
-      height: 620,
-      child: ListView(
-        children: [
-          Container(
-              height: 600,
-              width: 100,
-              child: Column(
-                children: <Widget>[
-                  Expanded(
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                            child: Row(
-                          children: <Widget>[
-                            Container(
-                                height: 50,
-                                child: CircleAvatar(
-                                  radius: 48,
-                                  backgroundImage: NetworkImage(
-                                      'https://t3.ftcdn.net/jpg/01/66/39/54/360_F_166395402_UcbaS5Z5Tj1rEbMvzhHR1U7DpCgCWd3r.jpg'),
-                                )),
-                            Container(
-                                height: 70,
-                                child: Center(
-                                  child: Text(
-                                    'stellar_bright888',
-                                    textAlign: TextAlign.center,
-                                  ),
-                                )),
-                            Spacer(
-                              flex: 2,
-                            ),
-                            Container(
-                                height: 70, child: Center(child: Text('...'))),
-                          ],
+      height: 600,
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Container(
+                    child: Row(
+                  children: <Widget>[
+                    Container(
+                        height: 50,
+                        child: CircleAvatar(
+                          radius: 48,
+                          backgroundImage: NetworkImage(item.image),
                         )),
-                        Container(
-                          height: 400,
-                          child: Image.network(
-                            'https://i.guim.co.uk/img/media/eb6caf6db81be662905ee1230f1eddc6a9831624/0_206_5472_3283/master/5472.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=0f2236596712182911c4fef6b47c55e2',
-                            fit: BoxFit.cover,
+                    Container(
+                        height: 70,
+                        child: Center(
+                          child: Text(
+                            item.name,
+                            textAlign: TextAlign.center,
                           ),
-                        ),
-                        Container(
-                          child: Text('Third  Row'),
-                        ),
-                      ],
+                        )),
+                    Spacer(
+                      flex: 2,
                     ),
-                  )
-                ],
-              )),
-          Container(
-              height: 300,
-              width: 100,
-              child: Column(
-                children: <Widget>[
-                  Expanded(
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          height: 60,
-                          child: Image.network(
-                            'https://upload.wikimedia.org/wikipedia/commons/4/46/Eden_Hazard_at_Baku_before_2019_UEFA_Europe_League_Final.jpg',
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        Text('Eden Hazard')
-                      ],
-                    ),
-                  )
-                ],
-              )),
-          Container(
-              height: 300,
-              width: 100,
-              child: Column(
-                children: <Widget>[
-                  Expanded(
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          height: 60,
-                          child: Image.network(
-                            'https://upload.wikimedia.org/wikipedia/commons/4/46/Eden_Hazard_at_Baku_before_2019_UEFA_Europe_League_Final.jpg',
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        Text('Eden Hazard')
-                      ],
-                    ),
-                  )
-                ],
-              )),
+                    Container(height: 70, child: Center(child: Text('...'))),
+                  ],
+                )),
+                Container(
+                  height: 400,
+                  child: Image.network(
+                    item.image,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Container(
+                  child: Text('Third  Row'),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
