3690e6ecec174968f51f80c1af2729530e7ce38d
Yean Seyha <yeanseyha7@gmail.com>
2023-04-20 09:55:13 +0700

    Added Single photo in instagram user

--- lib/instagram/pages/switch_tabs.dart
+++ lib/instagram/pages/switch_tabs.dart
@@ -8,7 +8,7 @@ class SwitchTabs extends StatelessWidget {
     return DefaultTabController(
       length: 2,
       child: Container(
-        height: 300,
+        height: 449,
         child: Column(
           children: [
             TabBar(tabs: [
@@ -25,10 +25,116 @@ class SwitchTabs extends StatelessWidget {
             Expanded(
               child: TabBarView(children: [
                 Container(
-                  child: Center(child: Text("First Tab1")),
+                  child: GridView.count(
+                    primary: false,
+                    padding: const EdgeInsets.all(20),
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    crossAxisCount: 3,
+                    children: <Widget>[
+                      Container(
+                        color: Colors.teal[100],
+                        child: Image.network(
+                          "https://upload.wikimedia.org/wikipedia/commons/4/46/210928_Jisoo.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        color: Colors.teal[100],
+                        child: Image.network(
+                          "https://static.independent.co.uk/2021/05/29/22/newFile.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        color: Colors.teal[100],
+                        child: Image.network(
+                          "https://i.dailymail.co.uk/i/pix/2017/05/31/14/40A0058900000578-0-image-a-186_1496238102575.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        color: Colors.teal[100],
+                        child: Image.network(
+                          "https://a.espncdn.com/photo/2021/0709/r878390_864x1296_2-3.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        color: Colors.teal[100],
+                        child: Image.network(
+                          "https://ftw.usatoday.com/wp-content/uploads/sites/90/2022/07/fifa-18-cristiano-ronaldo.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        color: Colors.teal[100],
+                        child: Image.network(
+                          "https://ftw.usatoday.com/wp-content/uploads/sites/90/2022/07/fifa-18-cristiano-ronaldo.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        color: Colors.teal[100],
+                        child: Image.network(
+                          "https://i.dailymail.co.uk/i/pix/2017/05/31/14/40A0058900000578-0-image-a-186_1496238102575.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        color: Colors.teal[100],
+                        child: Image.network(
+                          "https://a.espncdn.com/photo/2021/0709/r878390_864x1296_2-3.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        color: Colors.teal[100],
+                        child: Image.network(
+                          "https://ftw.usatoday.com/wp-content/uploads/sites/90/2022/07/fifa-18-cristiano-ronaldo.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        color: Colors.teal[100],
+                        child: Image.network(
+                          "https://ftw.usatoday.com/wp-content/uploads/sites/90/2022/07/fifa-18-cristiano-ronaldo.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        color: Colors.teal[100],
+                        child: Image.network(
+                          "https://i.dailymail.co.uk/i/pix/2017/05/31/14/40A0058900000578-0-image-a-186_1496238102575.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        color: Colors.teal[100],
+                        child: Image.network(
+                          "https://a.espncdn.com/photo/2021/0709/r878390_864x1296_2-3.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        color: Colors.teal[100],
+                        child: Image.network(
+                          "https://ftw.usatoday.com/wp-content/uploads/sites/90/2022/07/fifa-18-cristiano-ronaldo.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        color: Colors.teal[100],
+                        child: Image.network(
+                          "https://ftw.usatoday.com/wp-content/uploads/sites/90/2022/07/fifa-18-cristiano-ronaldo.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
                 Container(
-                  child: Center(child: Text("Second Tab2")),
+                  child: Center(child: Text("Photos and video of you")),
                 ),
               ]),
             )
